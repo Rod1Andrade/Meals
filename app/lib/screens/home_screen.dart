676e65d6b1c56c998583aa406611a84a9b552d1d
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2021-05-25 08:03:04 -0300

    UPDATE - Cleaning Default Boile Plate code:

---
+++ app/lib/screens/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// Home Screen
+///
+/// @author Rodrigo Andrade
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
