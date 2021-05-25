f2d181fc827da5cc008f479f8baf31a104d31f2d
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2021-05-25 11:54:53 -0300

    UPDATE - Relacionando categoria com refeicoes atraves do id

--- app/lib/screens/category_meals_screen.dart
+++ app/lib/screens/category_meals_screen.dart
@@ -1,4 +1,6 @@
+import 'package:app/data/dummy_data.dart';
 import 'package:app/models/category_model.dart';
+import 'package:app/models/meal_model.dart';
 import 'package:flutter/material.dart';
 
 /// Lista de receitas por categoria.
@@ -8,16 +10,26 @@ import 'package:flutter/material.dart';
 class CategoryMealScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Cateagorial de refeicoes retornada a partir da rota.
     final CategoryModel categoryModel =
         ModalRoute.of(context).settings.arguments as CategoryModel;
+
+    // Lista de Refeicoes.
+    final List<MealModel> meals = DUMMY_MEALS
+        .where((mealModel) => mealModel.categoriesId.contains(categoryModel.id))
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryModel.title),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text('Receitas por categoria...'),
+      body: ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (context, index) {
+          MealModel mealModel = meals[index];
+          return Text(mealModel.title);
+        },
       ),
     );
   }
